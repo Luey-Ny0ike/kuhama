58ed3e448a30d12ff672d5c07837a6a7aaf2a1e1
lewis nyoike <nyoikelewis@gmail.com>
2018-09-14 02:48:25 +0300

    Added all the routes for the user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
   get '/faq', to: "home#faq"
+
+  # User dashboard routes
+  get 'users', to: "users#index", as: "users"
+  get 'users/:id', to: "users#show", as: "user"
+  get 'users/:id/edit', to: "users#edit", as: "edit_user"
+  patch 'users/:id', to: "users#update"
 end
