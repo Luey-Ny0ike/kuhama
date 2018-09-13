329c4f65a67f69e7b2fc537c19079c17573269da
lewis nyoike <nyoikelewis@gmail.com>
2018-09-14 02:49:26 +0300

    Migrated the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180913232319) do
+ActiveRecord::Schema.define(version: 20180913234638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,12 @@ ActiveRecord::Schema.define(version: 20180913232319) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.integer "number"
+    t.string "website"
+    t.text "notes"
+    t.string "facebook"
+    t.string "twitter"
+    t.string "instagram"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
