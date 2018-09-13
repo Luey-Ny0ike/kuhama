9c95605ac4e0308a081a7fa35168908a44634513
lewis nyoike <nyoikelewis@gmail.com>
2018-09-14 00:43:33 +0300

    Migrated the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180913205055) do
+ActiveRecord::Schema.define(version: 20180913212438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20180913205055) do
     t.string "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "listing_category_id"
   end
 
   create_table "ratings", force: :cascade do |t|
