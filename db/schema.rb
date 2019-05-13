b31d50999614aa44f0ba0b50f2e51894f7332ef3
lewis nyoike <nyoikelewis@gmail.com>
2019-05-13 23:15:53 +0300

    Migrated the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_09_114054) do
+ActiveRecord::Schema.define(version: 2019_05_13_193412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,12 @@ ActiveRecord::Schema.define(version: 2019_05_09_114054) do
     t.integer "rooms"
     t.integer "bathrooms"
     t.integer "size"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.float "longitude"
+    t.float "latitude"
   end
 
   create_table "ratings", force: :cascade do |t|
