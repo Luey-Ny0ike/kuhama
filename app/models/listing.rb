ccdca680729e6f48d2f2010f5e8347311b46970c
lewis nyoike <nyoikelewis@gmail.com>
2019-05-13 23:12:40 +0300

    geocoder setup

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,9 @@
 class Listing < ApplicationRecord
+  def address
+    [street, city, state, country].compact.join(', ')
+  end
+  geocoded_by :address
+  after_validation :geocode 
   has_many :ratings
   belongs_to :listing_category
   belongs_to :user
