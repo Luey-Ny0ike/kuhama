ea18c5b1e37b711f3e866f64d614d12737991b49
lewis nyoike <nyoikelewis@gmail.com>
2019-05-23 21:56:23 +0300

    Fixes for switching to mysql

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_13_193412) do
+ActiveRecord::Schema.define(version: 2019_05_17_182705) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "active_storage_attachments", force: :cascade do |t|
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -25,7 +22,7 @@ ActiveRecord::Schema.define(version: 2019_05_13_193412) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -36,14 +33,14 @@ ActiveRecord::Schema.define(version: 2019_05_13_193412) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "ammenities", force: :cascade do |t|
+  create_table "ammenities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "listing_ammenities", force: :cascade do |t|
+  create_table "listing_ammenities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "listing_id"
     t.bigint "ammenity_id"
     t.datetime "created_at", null: false
@@ -52,13 +49,13 @@ ActiveRecord::Schema.define(version: 2019_05_13_193412) do
     t.index ["listing_id"], name: "index_listing_ammenities_on_listing_id"
   end
 
-  create_table "listing_categories", force: :cascade do |t|
+  create_table "listing_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "listings", force: :cascade do |t|
+  create_table "listings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "asset_name"
     t.text "description"
     t.string "pricing"
@@ -78,9 +75,10 @@ ActiveRecord::Schema.define(version: 2019_05_13_193412) do
     t.string "country"
     t.float "longitude"
     t.float "latitude"
+    t.string "main_category"
   end
 
-  create_table "ratings", force: :cascade do |t|
+  create_table "ratings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "listing_id"
     t.text "body"
     t.datetime "created_at", null: false
@@ -88,14 +86,14 @@ ActiveRecord::Schema.define(version: 2019_05_13_193412) do
     t.index ["listing_id"], name: "index_ratings_on_listing_id"
   end
 
-  create_table "testimonials", force: :cascade do |t|
+  create_table "testimonials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "body"
     t.string "starrating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -104,8 +102,8 @@ ActiveRecord::Schema.define(version: 2019_05_13_193412) do
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "first_name"
