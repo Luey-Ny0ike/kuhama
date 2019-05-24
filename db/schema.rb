534202ab27ba5861d117708257bfe4ad97de9bd0
lewis nyoike <nyoikelewis@gmail.com>
2019-05-24 20:37:04 +0300

    Migrated the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_064002) do
+ActiveRecord::Schema.define(version: 2019_05_24_163625) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -115,6 +115,8 @@ ActiveRecord::Schema.define(version: 2019_05_24_064002) do
     t.string "facebook"
     t.string "twitter"
     t.string "instagram"
+    t.boolean "user_role", default: true
+    t.boolean "superadmin_role", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
