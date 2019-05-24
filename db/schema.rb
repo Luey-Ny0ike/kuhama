56127ad506ce2b7fd00b86dbaf40796c87908a40
lewis nyoike <nyoikelewis@gmail.com>
2019-05-24 11:05:05 +0300

    Migrated the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_17_182705) do
+ActiveRecord::Schema.define(version: 2019_05_24_064002) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2019_05_17_182705) do
     t.float "longitude"
     t.float "latitude"
     t.string "main_category"
+    t.string "building_name"
   end
 
   create_table "ratings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
