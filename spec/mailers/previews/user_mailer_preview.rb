93321dc7794151d57c18a46abc4d8379a45d95fd
lewis nyoike <nyoikelewis@gmail.com>
2019-08-23 16:58:03 +0300

    Testing the mailer.

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
+  def payment_mailer
+    @payment = Payment.first
+    @subscription = @payment.user.all_follows.first 
+    UserMailer.with(payment: Payment.first).payment_mailer
+  end
 end
