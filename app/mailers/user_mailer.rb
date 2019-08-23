9e1edef5763c9bd39f69fa6cac8b3710d9f6018e
lewis nyoike <nyoikelewis@gmail.com>
2019-08-23 16:56:16 +0300

    Cracked how to return a specific record being followed by a user. WOOOHOO!!

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,8 @@
 class UserMailer < ApplicationMailer
 
   def payment_mailer
+    @payment = params[:payment]
+    @subscription = @payment.user.all_follows.first 
     mail(to: 'nyoikelewis@gmail.com', subject: 'New Subscription Paymnet')
   end
 end
