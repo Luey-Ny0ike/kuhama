0b83f9d15687ad5f8bd329c07d0ad427b17b4a7e
lewis nyoike <nyoikelewis@gmail.com>
2019-08-25 00:10:23 +0300

    Solved the order by descending order issue.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-      @listings = Listing.last(6)
+      @listings = Listing.order(id: :desc).last(6)
       @listing_categories = ListingCategory.all
   end
   def about_us
