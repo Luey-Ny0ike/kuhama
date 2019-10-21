824d85f7c4942c9b7a47ed0d37133fa2d79da9dd
lewis nyoike <nyoikelewis@gmail.com>
2019-10-21 12:08:35 +0300

    Bug fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 
   def payment_mailer
     @payment = params[:payment]
-    @subscription = @payment.user.all_follows.first 
-    mail(to: 'nyoikelewis@gmail.com', subject: 'New Subscription Paymnet')
+    @subscription = @payment.user.all_follows.first
+    mail(to: 'tusake.co.ke@gmail.com', subject: 'New Subscription Paymnet')
   end
 end
